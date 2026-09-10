3bf8d5465cac0f98ef1e56aa039ee6f6fb2b6092
Axmet1K <azaliya.akhmetzhan@mail.ru>
2026-09-10 23:33:49 +0700

    Создание класса и окна с виджетами

--- calculator.cpp
+++ calculator.cpp
@@ -1,21 +1,59 @@
 //==== Подключение библиотек ====
 #include "calculator.h"
-
+#include <QVBoxLayout>     // вертикальная раскладка
+#include <QHBoxLayout>     // горизонтальная раскладка
+#include <QWidget>         // базовый виджет
+#include <QMessageBox>     // для сообщений об ошибках
 
 //==== Конструктор ====
-Сalculator::Сalculator(QWidget *parent) //конструктор
+Calculator::Calculator(QWidget *parent) //конструктор
     : QMainWindow(parent) //вызывает конструктор родителя
 {
+    // настройка окна
+    this->setWindowTitle("Сложение чисел");
+    this->resize(600, 200);
 
+    //центральный виджет для layuot
+    QWidget *central = new QWidget(this);
+    this->setCentralWidget(central);
+
+    //создание виджетов
+    input1 = new QLineEdit(this);
+    input1->setPlaceholderText("Введите первое слогаемое");
+
+    input2 = new QLineEdit(this);
+    input2->setPlaceholderText("Введите второе слогаемое");
+
+    equalsButton = new QPushButton("=", this);
+
+    resultLabel = new QLabel("Результат", this);
+    resultLabel->setAlignment(Qt::AlignCenter);
+
+    //раскладка
+    QVBoxLayout *mainLayout = new QVBoxLayout(central);
+    QHBoxLayout *inputLayout = new QHBoxLayout();
+
+    inputLayout->addWidget(input1);
+    inputLayout->addWidget(new QLabel("+", this));
+    inputLayout->addWidget(input2);
+    inputLayout->addWidget(equalsButton);
+
+    mainLayout->addLayout(inputLayout);
+    mainLayout->addWidget(resultLabel);
+
+    //
+    //connect(equalsButton, &QPushButton::clicked,
+    //        this, &Calculator::onEqualsClicked());
 }
 
 //деструктор
-Сalculator::~Сalculator()
+Calculator::~Calculator()
 {
 
 }
 
-//==== Пользовательские функции ====
+
+
 
 
 
--- calculator.h
+++ calculator.h
@@ -2,22 +2,28 @@
 #define CALCULATOR_H
 //===== Подключение библиотек =====
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
 
 //==== Класс главного окна ====
-class  Сalculator : public QMainWindow
+class  Calculator : public QMainWindow
 {
     Q_OBJECT
 
 public:
     // конструктор - вызывается при создании окна
-    Сalculator(QWidget *parent = nullptr);
+    Calculator(QWidget *parent = nullptr);
     // деструктор - вызывается при закрытии окна
-    ~Сalculator();
+    ~Calculator();
 
 private slots: //секуия для слотов
-
+    //void onEqualsClicked(); //слот для кнопки
 
 private: //приватные поля класса
-
+    QLineEdit *input1; //первое поле ввода
+    QLineEdit *input2; //второе поле ввода
+    QLabel *resultLabel; //вывод результата
+    QPushButton *equalsButton; //кнопка =
 };
 #endif // CALCULATOR_H
