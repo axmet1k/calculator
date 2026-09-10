e2aff1720ae8b4ed49e364118bc38a5f0fcec602
Axmet1K <azaliya.akhmetzhan@mail.ru>
2026-09-11 00:17:08 +0700

    Добавлены комментарии в файле main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+//==== Подключение библиотек ====
 #include "calculator.h"
 
 #include <QApplication>
 
+//==== Главная функция ====
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
